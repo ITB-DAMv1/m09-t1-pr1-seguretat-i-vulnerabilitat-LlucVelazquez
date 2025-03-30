8a3eb695cc6dcb1a1b4034174cf0b0fbdc944ca4
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-03-30 16:15:41 +0200

    menu

--- t1-pr1-LlucVelazquez/Program.cs
+++ t1-pr1-LlucVelazquez/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace t1_pr1_LlucVelazquez
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+namespace t1_pr1_LlucVelazquez
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const string Msg = "Menu:" +
+                "\n1. Registre" +
+                "\n2. Verificacio de dades" +
+                "\n Encriptacio i desncriptacio amb RSA";
+
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine(Msg);
+                int num = int.Parse(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        // cosa
+                        break;
+                    case 2:
+                        // otra cosa
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
