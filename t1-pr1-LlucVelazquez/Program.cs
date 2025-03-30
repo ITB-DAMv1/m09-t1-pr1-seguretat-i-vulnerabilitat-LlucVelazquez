d1bb9f9cbebe3f4588483d94cabb3d9fe1e6004e
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-03-30 16:04:25 +0200

    create project and git ignore

---
+++ t1-pr1-LlucVelazquez/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace t1_pr1_LlucVelazquez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
